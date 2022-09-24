a34241d618b7f98c5a8ef58a3dca4662e7198a53
Paweł Dymek <melasa1998@gmail.com>
2022-09-24 18:44:58 +0200

    Połączenie z bazą danych

--- DatabaseApp/DatabaseApp/DatabaseApp/Controllers/AppointmentController.cs
+++ DatabaseApp/DatabaseApp/DatabaseApp/Controllers/AppointmentController.cs
@@ -2,13 +2,14 @@
 using System.Linq;
 using System.Collections.Generic;
 using DatabaseApp.Models;
-using Microsoft.AspNetCore.Http;
+using System.Data.SqlClient;
 
 namespace DatabaseApp.Controllers
 {
     public class AppointmentController : Controller
     {
-        public static AppointmentList appointmentList = new AppointmentList();
+        SqlConnection connection = new SqlConnection("Data Source=(localdb)\\Appointments;Initial Catalog=Appointments;Integrated Security=True");
+
         private static int appointmentNumber = -1;
         public IActionResult Index()
         {
@@ -20,30 +21,63 @@ namespace DatabaseApp.Controllers
         {
             if(name != null && surname != null && phone != null)
             {
-                Appointment appointment = new Appointment();
-                appointment.Name = name;
-                appointment.Surname = surname;
-                appointment.PhoneNumber = phone;
+                connection.Open();
+                string command = string.Format(
+                    "INSERT INTO Appointments(ID, Name, Surname, Phone_number) VALUES({0}, '{1}', '{2}', '{3}')", appointmentNumber, name, surname, phone);
+                SqlCommand query = new SqlCommand(command, connection);
+                int result = query.ExecuteNonQuery();
+                connection.Close();
                 appointmentNumber++;
-                appointment.Number = appointmentNumber;
-                if (!appointmentList.IsAlreadyInTheList(appointment))
-                    appointmentList.Add(appointment);
-            }            
-            return View(appointmentList);
+            }
+            
+            return View("FromForm");
         }
 
         public IActionResult FromForm()
         {
-            return View(appointmentList);
+            string command = "SELECT * FROM Appointments";
+            connection.Open();
+            SqlCommand query = new SqlCommand(command, connection);
+            SqlDataReader reader = query.ExecuteReader();
+            List<Appointment> appointments = new List<Appointment>();
+            while (reader.Read())
+            {
+                Appointment appointment = new Appointment()
+                {
+                    Number = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Surname = reader.GetString(2),
+                    PhoneNumber = reader.GetString(3)                    
+                };
+                appointments.Add(appointment);
+            }
+            return View(appointments);
         }
 
         //Usuwa zawsze ostatnią dodaną(albo pierwszą)
         [HttpPost]
         public IActionResult DeleteAppointment(string ID)
         {
-            Appointment appointment = appointmentList.appointments.SingleOrDefault(x => x.Number == int.Parse(ID));
-            appointmentList.appointments.Remove(appointment);
-            return View("FromForm", appointmentList);
+            string command = string.Format("DELETE FROM Appointments WHERE ID = {0}", ID);
+            connection.Open();
+            SqlCommand query = new SqlCommand(command, connection);
+            int result = query.ExecuteNonQuery();
+            string command1 = "SELECT * FROM Appointments";
+            SqlCommand query1 = new SqlCommand(command1, connection);
+            SqlDataReader reader = query.ExecuteReader();
+            List<Appointment> appointments = new List<Appointment>();
+            while (reader.Read())
+            {
+                Appointment appointment = new Appointment()
+                {
+                    Number = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Surname = reader.GetString(2),
+                    PhoneNumber = reader.GetString(3)
+                };
+                appointments.Add(appointment);
+            }
+            return View("FromForm", appointments);
         }
 
         public IActionResult Error()
